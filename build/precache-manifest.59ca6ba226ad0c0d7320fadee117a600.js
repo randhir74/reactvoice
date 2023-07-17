65e7e7e0144c8f0ec5f07a4e6a0d216ecfca0638
Piyush Kumar Sharma <pks05072001@gmail.com>
2023-07-17 22:48:17 +0530

    IntelliNews Initial

---
+++ build/precache-manifest.59ca6ba226ad0c0d7320fadee117a600.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5811bf25bf41043c836a28bc9d7a4925",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a2f0cad0e65da73428b",
+    "url": "/static/css/main.3c2fef4c.chunk.css"
+  },
+  {
+    "revision": "78f7bacd4ad77c7cc761",
+    "url": "/static/js/2.dd8efa81.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.dd8efa81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a2f0cad0e65da73428b",
+    "url": "/static/js/main.d8d15c93.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  },
+  {
+    "revision": "b0a1eb7cef456f10aa4ae48bbf9827d8",
+    "url": "/static/media/logo.b0a1eb7c.png"
+  }
+]);
