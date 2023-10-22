c575b6847b7a952b36885339289e86cffa2c2dfc
randhir74 <randhirkumar1062000@gmail.com>
2023-10-22 09:29:19 +0530

    This is the first commit, everything is working fine. Need to update some security related problem

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
 
   useEffect(() => {
     alanBtn({
-      key: "242754f622105188ed0c9359070fc74b2e956eca572e1d8b807a3e2338fdd0dc/stage",
+      key: "f0e2a949458b3c48c9c22b13685c563a2e956eca572e1d8b807a3e2338fdd0dc/stage",
       onCommand: ({ command, articles, number }) => {
         if (command === "newHeadlines") {
           setNewsArticles(articles);
