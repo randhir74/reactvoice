3f8c979e83ca0d128eecfd92babb6122873a66db
randhir74 <randhirkumar1062000@gmail.com>
2023-10-23 12:17:27 +0530

    Fix some security related files, need to remove some error

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import alanBtn from "@alan-ai/alan-sdk-web";
 import logo from "./images/logo.png";
 import { NewsCards, Modal } from "./components";
 import useStyles from "./styles";
-
+import config from "./config.json"
 const App = () => {
   const [activeArticle, setActiveArticle] = useState(0);
   const [newsArticles, setNewsArticles] = useState([]);
@@ -16,7 +16,8 @@ const App = () => {
 
   useEffect(() => {
     alanBtn({
-      key: "f0e2a949458b3c48c9c22b13685c563a2e956eca572e1d8b807a3e2338fdd0dc/stage",
+      // key: "f0e2a949458b3c48c9c22b13685c563a2e956eca572e1d8b807a3e2338fdd0dc/stage",
+      key:config.key,
       onCommand: ({ command, articles, number }) => {
         if (command === "newHeadlines") {
           setNewsArticles(articles);
